5ad34d6697b80b83b763774fd71f1ecea5e2f89e
anuragchillarige <pinganurag@gmail.com>
2022-06-12 16:11:29 -0700

    added the ability to accept an invalid location

--- src/renderer/Components/Weather.jsx
+++ src/renderer/Components/Weather.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 
+const IncorrectData = () => {
+  return (
+    <div className="weather">
+      <h2 className="noLocation">No Location Found</h2>
+    </div>
+  );
+};
+
 export default function Weather() {
   const [temp, setTemp] = useState();
   const [highTemp, setHighTemp] = useState();
   const [lowTemp, setLowTemp] = useState();
   const [img, setImg] = useState();
+  const [valid, setValid] = useState(true);
   const city = 'Cupertino';
 
   function convertF(celcius) {
@@ -12,17 +21,26 @@ export default function Weather() {
   }
 
   function setWeather(data) {
-    setTemp(convertF(data.main.temp));
-    setHighTemp(convertF(data.main.temp_max));
-    setLowTemp(convertF(data.main.temp_min));
-    setImg(`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    if (data.cod === '404') {
+      setValid(false);
+    } else {
+      setValid(true);
+      setTemp(convertF(data.main.temp));
+      setHighTemp(convertF(data.main.temp_max));
+      setLowTemp(convertF(data.main.temp_min));
+      setImg(`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    }
   }
 
-  const sds = fetch(
+  const api = fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=91ed74c2909e6d1d05ef3dd5569b5de2`
   )
     .then((response) => response.json())
     .then((data) => setWeather(data));
+
+  if (valid === false) {
+    return <IncorrectData />;
+  }
 
   return (
     <div className="weather">
