5057432c8b149054187a1e1b03294db223eccb59
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2022-06-13 17:01:04 -0700

    Messagesdnnfksjn

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,29 +1,27 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import Sign from './Sign';
-
-const Hello = () => {
-  return (
-    <div>
-      <Sign/>
-    </div>
-    
-  );
-=======
+// import Sign from './Sign';
 import Weather from './Components/Weather';
 import Time from './Components/Time';
 
+// const Hello = () => {
+//   return (
+//     <div>
+//       <Sign/>
+//     </div>
+    
+//   );
+
+
 const Hello = () => {
-  return <Weather />;
->>>>>>> 5ad34d6697b80b83b763774fd71f1ecea5e2f89e
+  return( <Weather />);
 };
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Weather />} />
       </Routes>
     </Router>
   );
